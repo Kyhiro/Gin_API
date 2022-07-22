f061ddc8f08df1d51c61473032b5f1eb52332eeb
AnhTT <anhtt@kaiyouit.com>
2022-07-22 17:56:05 +0700

    [Add] - First commit

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"GinAPI/Config"
+	"GinAPI/Models"
+	"GinAPI/Routes"
+	"fmt"
+	"github.com/jinzhu/gorm"
+)
+
+var err error
+
+func main() {
+	Config.DB, err = gorm.Open("mysql", Config.DbURL(Config.BuildDBConfig()))
+	if err != nil {
+		fmt.Println("Status:", err)
+	}
+	defer Config.DB.Close()
+	Config.DB.AutoMigrate(&Models.User{})
+	r := Routes.SetupRouter()
+	//running
+	r.Run()
+}
